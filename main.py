6febf0ff5cc7e1ac059a29152c158ea4ad65a9d8
Raymund Santillan <raymsantillan@gmail.com>
2020-05-29 02:13:12 +0800

    Implement and identify subnet mask based on the given cidr input

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 
 from terminaltables import AsciiTable
 
+octal_equivalents = [128, 64, 32, 16, 8, 4, 2, 1]
+ip_blocks = 4
+
 def main():
-    octal_equivalents = [256,128,64,32,16,8,4,2,1]
-    ip_blocks = 4
+       
     no_of_host = 254
     network_id = "192.168.10.10"
     broadcast_ip = "192.168.10.10"
@@ -11,19 +13,62 @@ def main():
     last_ip = "192.168.10.2"
     subnet_mask = "255.255.255.254"
 
+    ip_address = input("IP Address with CIDR (e.g 192.168.10.10/24):")
+    cidr = ip_address.split("/")[1]
+
+    # identify the subnet mask
+    # e.g 24
+    # so 24 "1" bits are turned on
+    # on the given ip address
+
+    # first is to convert the CIDR into its binary representation
+    subnet_mask = cidr_to_binary(int(cidr))
+
     table_data = [
-        ['CIDRs', '/24','/27','/30'],
-        ['Subnet', '255.255.255.255'],
+        ['I.P ADDRESS', ip_address],
+        ['CIDR', '/' + cidr ],
+        ['Subnet Mask', subnet_mask],
         ['Network ID ', '192.168.2.142'],
         ['Broadcast ID ', '192.168.2.142'],
-        ['First IP', '192.168.2.142'],
-        ['Last IP ', '192.168.2.142']
+        ['First I.P', '192.168.2.142'],
+        ['Last I.P ', '192.168.2.142']
     ]
 
-    table = AsciiTable(table_data)
+    table = AsciiTable(table_data,None)
+    print("SUBNET TABLE FOR ",ip_address)
     print(table.table)
 
-    # ip_address = input("IP Address:")
+def cidr_to_binary(cidr):
+    subnet_binary = []
+    subnet_octets = []
+    
+    # turn on the "1" bits based on the cidr
+    subnet_binary[0:cidr] = [1] * cidr
+    # turn on the "0" bits after the cidr range
+    subnet_binary[cidr:32] = [0] * cidr
+    subnet_octets.append(subnet_binary[0:8])
+    subnet_octets.append(subnet_binary[8:16])
+    subnet_octets.append(subnet_binary[16:24])
+    subnet_octets.append(subnet_binary[24:32])
+
+    # print(subnet_octets)
+    # print(second_octet)
+    # print(third_octet)
+    # print(fourth_octet)
+
+    # get the subnet mask
+    subnet_mask = ''
+    for x in range(len(subnet_octets)):
+        temp_sum = 0
+        for i in range(0,8):
+            if subnet_octets[x][i] > 0:
+                temp_sum += octal_equivalents[i]
+        subnet_mask += str(temp_sum) + "."
+
+    return subnet_mask
+
+main()
+
     # cidrs = list(map(int, input("CIDR/s: ").split()))
 
     # print("IP Address: ",ip_address)
@@ -36,7 +81,7 @@ def main():
 
     #     # e.g 24
     #     # so 24 "1" bits are turned on
-    #     # on the given ip address
+    #     # on the given ip addressr5
     #     # for index in range(0,ip_blocks):
     #     #     print(sum(octal_equivalents[0:int(cidr)]))
 
@@ -50,7 +95,7 @@ def main():
     #     print("First IP:",first_ip)
     #     print("Last IP:",last_ip)
 
-def convert_ip_to_binary(ip_address):
+# def convert_ip_to_binary(ip_address):
 
     # octal_equivalents = [128, 64, 32, 16, 8, 4, 2, 1]
     # octets = ip_address.split(".")
@@ -101,7 +146,5 @@ def convert_ip_to_binary(ip_address):
         #     dividend = quotient
         
         # print("Remainder for octet " + octet  + ":", remainder)
-        return ip_address
+        # return ip_addres
 
-
-main()
